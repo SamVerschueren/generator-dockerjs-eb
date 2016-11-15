30abd0005ae07c9559785ee9903a6a865d2a9cf3
Sam Verschueren <sam.verschueren@gmail.com>
2016-11-15 11:05:19 +0100

    es2015ify

--- app/index.js
+++ app/index.js
@@ -1,90 +1,67 @@
 'use strict';
+const path = require('path');
+const yeoman = require('yeoman-generator');
+const moment = require('moment');
+const uppercamelcase = require('uppercamelcase');
 
-/**
- * Yeoman generator for Docker Elastic Beanstalk applications.
- *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
- * @since  25 Sep. 2015
- */
+module.exports = class extends yeoman.Base {
 
-// module dependencies
-var path = require('path'),
-    yeoman = require('yeoman-generator'),
-    moment = require('moment'),
-    uppercamelcase = require('uppercamelcase');
+	init() {
+		return this.prompt([
+			{
+				name: 'functionName',
+				message: 'What do you want to name project?',
+				default: uppercamelcase(process.cwd().split(path.sep).pop())
+			},
+			{
+				name: 'functionDescription',
+				message: 'What\'s the description of the project?',
+				validate: val => val.length > 0 ? true : 'You have to provide a description'
+			},
+			{
+				name: 'keywords',
+				message: 'Provide a list of keywords?',
+				filter: keywords => keywords.replace(/,? /g, ',').split(',')
+			},
+			{
+				name: 'port',
+				message: 'What\'s the port the application will be runnig on?',
+				default: 8080,
+				type: 'Number',
+				validate: input => input && (!isNaN(input) || input.match(/[1-9][0-9]+/))
+			},
+			{
+				name: 'githubUsername',
+				message: 'What is your GitHub username?',
+				store: true,
+				validate: val => val.length > 0 ? true : 'You have to provide a username'
+			}
+		]).then(props => {
+			// Build up the template
+			const tpl = {
+				functionName: props.functionName,
+				functionDescription: props.functionDescription,
+				keywords: props.keywords,
+				port: props.port,
+				name: this.user.git.name(),
+				email: this.user.git.email(),
+				year: moment().year()
+			};
 
-module.exports = yeoman.generators.Base.extend({
-    init: function () {
-        var done = this.async();
+			const mv = (from, to) => {
+				this.fs.move(this.destinationPath(from), this.destinationPath(to));
+			};
 
-        // Ask the questions
-        this.prompt([
-            {
-                name: 'functionName',
-                message: 'What do you want to name project?',
-                default: uppercamelcase(process.cwd().split(path.sep).pop())
-            },
-            {
-                name: 'functionDescription',
-                message: 'What\'s the description of the project?',
-                validate: function (val) {
-                    return val.length > 0 ? true : 'You have to provide a description';
-                }
-            },
-            {
-                name: 'keywords',
-                message: 'Provide a list of keywords?',
-                filter: function (keywords) {
-                    return keywords.replace(/,? /g, ',').split(',');
-                }
-            },
-            {
-                name: 'port',
-                message: 'What\s the port the application will be runnig on?',
-                default: 8080,
-                type: 'Number',
-                validate: function (input) {
-                    return input && (!isNaN(input) || input.match(/[1-9][0-9]+/));
-                }
-            },
-            {
-                name: 'githubUsername',
-                message: 'What is your GitHub username?',
-                store: true,
-                validate: function (val) {
-                    return val.length > 0 ? true : 'You have to provide a username';
-                }
-            }
-        ], function (props) {
-            // Build up the template
-            var tpl = {
-                functionName: props.functionName,
-                functionDescription: props.functionDescription,
-                keywords: props.keywords,
-                port: props.port,
-                name: this.user.git.name(),
-                email: this.user.git.email(),
-                year: moment().year()
-            };
+			// Copy the template files
+			this.fs.copyTpl(`${this.templatePath()}/**`, this.destinationPath(), tpl);
 
-            var mv = function (from, to) {
-                this.fs.move(this.destinationPath(from), this.destinationPath(to));
-            }.bind(this);
+			// Rename the files
+			mv('app/_package.json', 'app/package.json');
+		});
+	}
 
-            // Copy the template files
-            this.fs.copyTpl(this.templatePath() + '/**', this.destinationPath(), tpl);
-
-            // Rename the files
-            mv('app/_package.json', 'app/package.json');
-
-            // We are done!
-            done();
-        }.bind(this));
-    },
-    install: function () {
-        // Install node dependencies
-        this.npmInstall(undefined, {
-            cwd: 'app'
-        });
-    }
-});
+	install() {
+		// Install node dependencies
+		this.npmInstall(undefined, {cwd: 'app'});
+	}
+};
